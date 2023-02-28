e6125de0d5ced35a3295796429429bc6c26efd11
TeksMarques <taotaodoce@gmail.com>
2023-02-28 20:48:29 -0300

    requisito 10 em andamento

--- app/backend/src/middleware/validationLogin.ts
+++ app/backend/src/middleware/validationLogin.ts
@@ -1,9 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
 
+// function isValidEmail(email: string) {
+//   const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+//   return regex.test(email);
+// }
+
+// function isValidPassword(password: string) {
+//   return password.length >= 6;
+// }
+
 function validationLogin(req: Request, res: Response, next: NextFunction) {
   const { email, password } = req.body;
   if (!email || !password) return res.status(400).json({ message: 'All fields must be filled' });
   next();
+
+  // if (!isValidEmail(email) || !isValidPassword(password)) {
+  //   return res.status(401).json({ message: 'Invalid email or password' });
+  // }
 }
 
 export default validationLogin;
