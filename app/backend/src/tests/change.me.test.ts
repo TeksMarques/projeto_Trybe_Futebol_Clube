6d02a39e92ece6d7b82ee030d994d4e579b2a1d4
TeksMarques <taotaodoce@gmail.com>
2023-02-28 15:21:34 -0300

    Testes da Secção Teams

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -1,45 +1,73 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
+import { Model } from 'sequelize';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
 import Example from '../database/models/ExampleModel';
-
 import { Response } from 'superagent';
+import TeamService from '../services/TeamService';
+import TeamModel from '../database/models/TeamsModel';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
+describe('Testes da seção Teams', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
 
-  // let chaiHttpResponse: Response;
+  it('Testando camada service - getTeams', async () => {
 
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
+    const outPutTeams :TeamModel[] = [ new TeamModel({ id: 1, teamName: 'Teste' }) ];
 
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
+    sinon.stub(TeamModel, 'findAll').resolves(outPutTeams);
+    const teamService = new TeamService();
+    
+    const result = await teamService.getTeams();
+    
+    expect(result).to.be.deep.eq(outPutTeams);
+    expect(result.length).to.be.eq(1);
+    expect(result).to.be.an('array');
+  });
 
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
+  it('Testando camada service - getTeamById', async () => {
+    const outPutTeam :TeamModel = new TeamModel({ id: 1, teamName: 'Teste' });
 
-  //   expect(...)
-  // });
+    sinon.stub(TeamModel, 'findByPk').resolves(outPutTeam);
+    const teamService = new TeamService();
+    
+    const result = await teamService.getTeamById(1);
+    
+    expect(result).to.be.deep.eq(outPutTeam);
+    expect(result).to.be.an('object');
+  });
 
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
+  it('Testando camada controler - getTeams', async () => {
+    const outPutTeams :TeamModel[] = [ new TeamModel({ id: 1, teamName: 'Teste' }) ];
+
+    sinon.stub(TeamModel, 'findAll').resolves(outPutTeams);
+    const teamService = new TeamService();
+    
+    const result = await chai.request(app).get('/teams');
+    
+    expect(result).to.be.an('object');
+    expect(result.body).to.be.an('array');
+    expect(result.body).to.be.deep.eq(outPutTeams);
+  });
+
+  it('Testando camada controler - getTeamById', async () => {
+    const outPutTeam :TeamModel = new TeamModel({ id: 1, teamName: 'Teste' });
+
+    sinon.stub(TeamModel, 'findByPk').resolves(outPutTeam);
+    const teamService = new TeamService();
+    
+    const result = await chai.request(app).get('/teams/1');
+    
+    expect(result).to.be.an('object');
+    expect(result.body).to.be.an('object');
+    expect(result.body).to.be.deep.eq(outPutTeam);
   });
 });
